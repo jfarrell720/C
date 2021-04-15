9482716927cf5951ec132784af1d76f316420c12
jfarrell720 <71271254+jfarrell720@users.noreply.github.com>
2021-04-15 15:45:41 -0400

    teret
    tete

---
+++ BookApi/BookApi/Controllers/BookSearchController.cs
@@ -0,0 +1,36 @@
+﻿using BookApi.Models;
+using BookApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookSearchController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookSearchController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet("/SearchBooks")]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+
+        [HttpGet("/SearchBookId")]
+        public async Task<ActionResult<Book>> GetBooks(int id)
+        {
+            
+            return await _bookRepository.get(id);
+        }
+    }
+}
---
+++ BookApi/BookApi/Controllers/BooksController.cs
@@ -0,0 +1,51 @@
+﻿using BookApi.Models;
+using BookApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        //method get books
+        [HttpGet("/LoadBooks")]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+
+        [HttpPost("/GetBooks")]
+        public async Task<ActionResult<Book>> PostBooks([FromBody] Book book)
+        {
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutBooks(int id, [FromBody] Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            await _bookRepository.Update(book);
+
+            return NoContent();
+        }
+    }
+}
