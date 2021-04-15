7d7b688b106f0b9468ab31a79bfde120f751caeb
jfarrell720 <71271254+jfarrell720@users.noreply.github.com>
2021-04-15 16:10:03 -0400

    Book Repository
    text

---
+++ BookRepository.cs
@@ -0,0 +1,56 @@
+﻿using BookApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        //Inject will query with Book Context
+        private readonly BookContext _context;
+
+        public BookRepository(BookContext context)
+        {
+            _context = context;
+        }
+        public async Task<Book> Create(Book book)
+        {
+            _context.Books.Add(book);
+            //will add data to the data base
+            await _context.SaveChangesAsync();
+
+            return book;
+        }
+
+        public async Task Delete(int id)
+        {
+            var bookToDelete = await _context.Books.FindAsync(id);
+            _context.Books.Remove(bookToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Book>> Get()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        public async Task<Book> get(int id)
+        {
+            return await _context.Books.FindAsync(id);
+        }
+
+        public async Task<Book> Get(string title)
+        {
+            return await _context.Books.FindAsync(title);
+        }
+
+        public async Task Update(Book book)
+        {
+            _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ IBookRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookApi.Models;
+
+namespace BookApi.Repositories
+{
+   public interface IBookRepository
+    {
+        //get one book 
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(string title);
+        //Get one book back with our id
+        Task<Book> get(int id);
+        //Create our book
+        Task<Book> Create(Book book);
+        //Update our book
+        Task Update(Book book);
+
+        Task Delete(int id);
+    }
+}
